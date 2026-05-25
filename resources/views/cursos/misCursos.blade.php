e6f9fdd4d9a80162095641c6b42d030d89c58532
wuiza-dev <luiska3006@gmail.com>
2026-05-24 23:04:16 -0500

    Cambio de logo en mis cursos

--- resources/views/cursos/misCursos.blade.php
+++ resources/views/cursos/misCursos.blade.php
@@ -127,14 +127,11 @@
             <div class="header-inner">
                 <a href="{{ url('/cursos') }}" class="logo-link">
                     <div class="logo-icon">
-                        <svg viewBox="0 0 24 24" aria-hidden="true">
-                            <path
-                                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-                        </svg>
+                        <img src="{{ asset('images/Logo_Universidad-Santiago-de-Cali.png') }}" alt="Logo USC">
                     </div>
                     <div class="logo-text">
-                        <span class="logo-title">Bienestar USC</span>
-                        <span class="logo-subtitle">Oferta de cursos</span>
+                        <span class="logo-title">SIBIU</span>
+                        <span class="logo-subtitle">Bienestar USC</span>
                     </div>
                 </a>
 
